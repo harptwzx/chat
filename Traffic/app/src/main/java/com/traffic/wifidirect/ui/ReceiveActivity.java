f59b4b1a5a74499e49958b43eba47cc985daf7ad
harptwzx <wzx123_snake@qq.com>
2026-05-30 02:09:33 +0000

    更新 ReceiveActivity.java

--- Traffic/app/src/main/java/com/traffic/wifidirect/ui/ReceiveActivity.java
+++ Traffic/app/src/main/java/com/traffic/wifidirect/ui/ReceiveActivity.java
@@ -1,15 +1,30 @@
 package com.traffic.wifidirect.ui;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.traffic.wifidirect.R;
+import com.traffic.wifidirect.WiFiDirectManager;
 
-public class ReceiveActivity extends Activity {
+public class ReceiveActivity extends AppCompatActivity {
+    private WiFiDirectManager wifiManager;
+    private TextView statusText;
+    private Button receiveButton;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive);
-        Toast.makeText(this, "接收文件功能开发中", Toast.LENGTH_SHORT).show();
-        // TODO: 二维码扫码+WiFi Direct加网+文件接收
+        
+        wifiManager = new WiFiDirectManager(this);
+        statusText = findViewById(R.id.status);
+        receiveButton = findViewById(R.id.btn_receive);
+        
+        receiveButton.setOnClickListener(v -> {
+            wifiManager.discoverPeers();
+            statusText.setText("等待发送方连接...");
+            // TODO: 启动接收服务
+        });
     }
 }
